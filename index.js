9b1e11d1f4894617af6a61186ddda143412c6f0c
Jaden Lund <lundjaden@gmail.com>
2022-07-18 17:36:56 -0500

    commit tree db0ba04a12aad426f1678181c7c17ffc9b4013e5

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var num3 = 22;
+var num4 = 6;
+var multiply = num1 * num2;
+
+
+var random = Math.ceil(Math.random());
+
+
+var mod = num3 % num4;
+
+var max = Math.max(1, 4, 14, 20);
